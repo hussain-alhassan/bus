dc50ac0dd10ed255921551cab483451409ba3ec9
Basil <alrashedbasil@gmail.com>
2020-03-20 00:24:18 +0300

    removed the burger icon

--- resources/views/layouts/agentLayout.blade.php
+++ resources/views/layouts/agentLayout.blade.php
@@ -68,7 +68,6 @@
             <div class="navbar-header">
                 <a class="navbar-brand" href="{{ route('home') }}"><img src="{{asset('images/logo.png')}}" alt="Logo"></a>
                 <a class="navbar-brand hidden" href="{{ route('home') }}"><img src="{{asset('images/logo2.png')}}" alt="Logo"></a>
-                <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
             </div>
         </div>
         <div class="top-right">
