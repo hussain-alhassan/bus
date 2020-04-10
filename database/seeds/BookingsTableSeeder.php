a4dbbba934d9ee9c6c8a10a78d0ecbfc30210e0d
Basil <alrashedbasil@gmail.com>
2020-04-10 16:41:46 +0300

    added bookings seeds

--- database/seeds/BookingsTableSeeder.php
+++ database/seeds/BookingsTableSeeder.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
+use App\User;
+use App\City;
+use App\Office;
 
 class BookingsTableSeeder extends Seeder
 {
@@ -36,5 +39,67 @@ class BookingsTableSeeder extends Seeder
                 ],
             ]
         );
+
+        //Bookings
+        DB::table('bookings')->insert([
+                [
+                    'user_id' => User::where('email', 'a@a.com')->get('id')->first()->id,
+                    'from_city_id' => City::where('name', 'الأحساء')->get('id')->first()->id,
+                    'to_city_id' => City::where('name', 'دبي')->get('id')->first()->id,
+                    'office_id' => Office::where('agency_id', Agency::where('name_en', 'Rumailah')->get('id')->first()->id)->get('id')->first()->id,
+                    'depart' => Carbon::next(7),
+                    'seats' => '1',
+                    'status' => 'Confirmed',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ],
+                [
+                    'user_id' => User::where('email', 'b@b.com')->get('id')->first()->id,
+                    'from_city_id' => City::where('name', 'الأحساء')->get('id')->first()->id,
+                    'to_city_id' => City::where('name', 'دبي')->get('id')->first()->id,
+                    'office_id' => Office::where('agency_id', Agency::where('name_en', 'Rumailah')->get('id')->first()->id)->get('id')->first()->id,
+                    'depart' => Carbon::next(2),
+                    'seats' => '1',
+                    'status' => 'Pending',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ],
+                [
+                    'user_id' => User::where('email', 'c@c.com')->get('id')->first()->id,
+                    'from_city_id' => City::where('name', 'الأحساء')->get('id')->first()->id,
+                    'to_city_id' => City::where('name', 'دبي')->get('id')->first()->id,
+                    'office_id' => Office::where('agency_id', Agency::where('name_en', 'Rumailah')->get('id')->first()->id)->get('id')->first()->id,
+                    'depart' => Carbon::next(1),
+                    'seats' => '1',
+                    'status' => 'Rejected',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ],
+                [
+                    'user_id' => User::where('email', 'd@d.com')->get('id')->first()->id,
+                    'from_city_id' => City::where('name', 'الأحساء')->get('id')->first()->id,
+                    'to_city_id' => City::where('name', 'دبي')->get('id')->first()->id,
+                    'office_id' => Office::where('agency_id', Agency::where('name_en', 'Rumailah')->get('id')->first()->id)->get('id')->first()->id,
+                    'depart' => Carbon::next(5),
+                    'seats' => '1',
+                    'status' => 'Confiremed',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ],
+                [
+                    'user_id' => User::where('email', 'e@e.com')->get('id')->first()->id,
+                    'from_city_id' => City::where('name', 'الأحساء')->get('id')->first()->id,
+                    'to_city_id' => City::where('name', 'دبي')->get('id')->first()->id,
+                    'office_id' => Office::where('agency_id', Agency::where('name_en', 'Rumailah')->get('id')->first()->id)->get('id')->first()->id,
+                    'depart' => Carbon::next(9),
+                    'seats' => '3',
+                    'status' => 'Pending',
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now()
+                ],
+            ]
+        );
+
+        
     }
 }
