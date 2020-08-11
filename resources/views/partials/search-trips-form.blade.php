418f0c17a5c35a6a3f9fe52f5a742591d1c30116
Hussain <e.alhassan.h@gmail.com>
2020-08-11 09:14:16 -0400

    Fix for search trips old value

--- resources/views/partials/search-trips-form.blade.php
+++ resources/views/partials/search-trips-form.blade.php
@@ -22,7 +22,8 @@
                         <label class="col-form-label text-md-right">From</label>
                         <select class="form-control" name="from">
                             @foreach($cities as $city)
-                                <option value="{{$city->id}}" {{$city->id === intval(old('from')) ? 'selected' : ''}}>
+                                <option value="{{$city->id}}"
+                                    {{ (old('from') == $city->id ||  Request::get('from') == $city->id) ? 'selected' : ''}}>
                                     {{$city->name}}
                                 </option>
                             @endforeach
@@ -33,7 +34,8 @@
                         <label class="col-form-label text-md-right">To</label>
                         <select class="form-control" name="to">
                             @foreach($cities as $city)
-                                <option value="{{$city->id}}" {{$city->id === intval(old('to')) ? 'selected' : ''}}>
+                                <option value="{{$city->id}}"
+                                    {{ (old('to') == $city->id || Request::get('to') == $city->id) ? 'selected' : ''}}>
                                     {{$city->name}}
                                 </option>
                             @endforeach
@@ -44,20 +46,20 @@
                         <label class="col-form-label text-md-right">Depart</label>
 
                         <input type="date" class="form-control @error('depart') is-invalid @enderror"
-                               name="depart" value="{{ old('depart') ?? Request::get('depart') }}" required autocomplete="depart">
+                               name="depart" value="{{ old('depart') ?? Request::get('depart') }}" required>
                     </div>
 
                     <div class="mr-3 flex-fill">
                         <label class="col-form-label text-md-right">Return</label>
                         <input type="date" class="form-control @error('return') is-invalid @enderror"
-                               name="return" value="{{ Request::get('return') }}" autocomplete="return">
+                               name="return" value="{{ old('return') ?? Request::get('return') }}">
                     </div>
 
                     <div class="mr-3 flex-fill">
                         <label class="col-form-label text-md-right">Seats</label>
                         <select name="seats" class="browser-default custom-select @error('seats') is-invalid @enderror">
                             @for($i = 1; $i <= 10; $i++)
-                                <option value="{{$i}}" {{ Request::get('seats') == $i ? 'selected' : ''}}>{{$i}}</option>
+                                <option value="{{$i}}" {{ (old('seats') == $i || Request::get('seats') == $i) ? 'selected' : ''}}>{{$i}}</option>
                             @endfor
                         </select>
                     </div>
